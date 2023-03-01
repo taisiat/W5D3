9d0272a814e12c5bc26e9fa3ac7d908738cadf02
Ahmed <ahmed96razi39@gmail.com>
2023-03-01 11:30:32 -0800

    commit before lunch

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,4 @@
+PRAGMA foreign_keys = ON;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -32,7 +33,7 @@ CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     body TEXT NOT NULL,
     question_id INTEGER NOT NULL,
-    parent_reply_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
     author_id INTEGER NOT NULL,
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
@@ -46,5 +47,38 @@ CREATE TABLE replies (
 -- 3          "thanks"                          id(of qest)      2              tk
 
 CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
 
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO 
+    users (fname, lname)
+VALUES 
+    ('Ahmed', 'R'), 
+    ('Taisia', 'K');
+
+INSERT INTO 
+    questions (title, body, user_id)
+VALUES 
+    ('OOP', 'What is OOP?', 2);
+
+INSERT INTO 
+    question_follows (question_id, user_id)
+VALUES 
+    (1, 2);
+
+INSERT INTO 
+    replies (body, question_id, parent_reply_id, author_id)
+VALUES 
+    ('OOP is Obje,, Or,, Pro,,', 1, null, 1);
+
+INSERT INTO 
+    question_likes (question_id, user_id)
+VALUES 
+    (1, 2);
+
+    
